b1588c421ca2f75dde496a53e3dcd3efc5d46d70
Will <wp26@buffalo.edu>
2020-07-20 07:23:40 -0400

    Task #42 Create verification code entry page

--- src/scenes/verificiation-code-page/verification-code-page.js
+++ src/scenes/verificiation-code-page/verification-code-page.js
@@ -11,38 +11,29 @@ import OnboardingTitle from "_atoms/onboarding-page-title";
 import InfoText from "_atoms/info-text";
 import AssistButton from "_atoms/assist-button";
 import ContinueButton from "_atoms/continueButton";
-import NumberEntry from "_atoms/number-entry";
 import * as firebase from "firebase";
+import InputResponse from "_atoms/input-response";
 
-function getUserID(phoneNumber) {
-  fetch(
-    "https://www-student.cse.buffalo.edu/peek_mobile_dating/userIDLookUp.php",
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        phoneNumber: phoneNumber,
-      }),
-    }
-  )
-    .then((response) => response.json())
-    .then((responseJson) => {
-      const userID = JSON.parse(responseJson);
-      return userID;
-    });
-}
+/**
+ * @author - William Phillips
+ * @param {*} route - route is a prop that allows me to recieve data passed from a previous page
+ * @param {*} navigation - prop that allows me to navigate to other pages and send data to those pages
+ * @description - functions displays the screen of the of the Verification code page
+ *              - it displays a text input box which requires the user to enter the security code sent to them via SMS
+ */
 
 function VerificationCodePage({ route, navigation }) {
   let { verificationId } = route.params;
   let { phoneNumber } = route.params;
-  let userID = "";
   phoneNumber = JSON.parse(JSON.stringify(phoneNumber));
   const [verificationCode, setVerificationCode] = React.useState();
   verificationId = JSON.parse(JSON.stringify(verificationId));
 
+  /**
+   *  @author - William Phillips
+   *  @description - takes users inputted verifcation code and their verification ID and
+   *               - authenticates if the verifcation code is valid
+   */
   async function verifyCode() {
     try {
       const credential = firebase.auth.PhoneAuthProvider.credential(
@@ -50,14 +41,41 @@ function VerificationCodePage({ route, navigation }) {
         verificationCode
       );
       await firebase.auth().signInWithCredential(credential);
-      Alert.alert("Phone authentication successful 👍");
-      userID = getUserID(phoneNumber);
-      //navigate to next page here
+
+      await getUserID(phoneNumber);
     } catch (err) {
       Alert.alert(`Error: ${err.message}`);
     }
   }
 
+  /**
+   * @author - William Phillips
+   * @param {*} phoneNumber - user inputted phone number
+   * @description - calls database api to create an entry of a user via phone number and returns back their userID
+   *              - function then navigates to name entry page
+   */
+  async function getUserID(phoneNumber) {
+    await fetch(
+      "https://www-student.cse.buffalo.edu/peek_mobile_dating/userIDLookUp.php",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          phoneNumber: phoneNumber,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const userID = JSON.parse(responseJson);
+        navigation.navigate("NameEntryPage", {
+          userID: userID,
+        });
+      });
+  }
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={"dark-content"} />
@@ -77,11 +95,12 @@ function VerificationCodePage({ route, navigation }) {
           <AssistButton description="Didn't recieve a code? " />
         </View>
         <View style={styles.inputResponse}>
-          <NumberEntry
+          <InputResponse
             _placeHolder="123456"
             _editable={!!verificationId}
-            _number={verificationCode}
-            _setNumber={setVerificationCode}
+            _onChangeText={verificationCode}
+            _onChangeCallBack={setVerificationCode}
+            _keyboardType={"numeric"}
           />
         </View>
       </View>
