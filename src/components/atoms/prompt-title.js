d670aa62735dd17e259db5c80e3682bd3a9286ac
Will <wp26@buffalo.edu>
2020-08-07 14:55:41 -0400

    created prompt title component

---
+++ src/components/atoms/prompt-title.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import { useFonts, ReemKufi_400Regular } from "@expo-google-fonts/reem-kufi";
+
+function PromptTitle({ description }) {
+  let [fontsLoaded] = useFonts({
+    ReemKufi_400Regular,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Text style={styles.textStyle}>{description}</Text>;
+  }
+}
+
+export default PromptTitle;
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: "#000000",
+    fontSize: 22,
+    fontFamily: "ReemKufi_400Regular",
+    marginLeft: 20,
+  },
+});
