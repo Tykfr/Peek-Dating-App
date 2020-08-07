1b8d033a6248918cddfa69a19d8814cc5c7b140d
Will <wp26@buffalo.edu>
2020-08-07 15:15:22 -0400

    updated naviagtion system to SignUpNavigator

--- src/index.js
+++ src/index.js
@@ -1,77 +1,7 @@
-import "react-native-gesture-handler";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import {
-  StartPage,
-  LoginPage,
-  NumberEntryPage,
-  VerificationCodePage,
-  NameEntryPage,
-  EmailEntryPage,
-  BirthdayEntryPage,
-  GenderIdentityPage,
-  DatingInterestPage,
-  SchoolEntryPage,
-  PoliticalEntryPage,
-  ReligionEntryPage,
-  EthnicityEntryPage,
-  OccupationEntryPage,
-  LocationEntryPage,
-  BioEntryPage,
-  PhotoSelectionPage,
-  PromptEntryPage,
-} from "_scenes/";
-const Stack = createStackNavigator();
+import { SignUpNavigator } from "_navigations";
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="StartPage"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="StartPage" component={StartPage} />
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="NumberEntryPage" component={NumberEntryPage} />
-        <Stack.Screen
-          name="VerificationPage"
-          component={VerificationCodePage}
-        />
-        <Stack.Screen name="NameEntryPage" component={NameEntryPage} />
-        <Stack.Screen name="EmailEntryPage" component={EmailEntryPage} />
-        <Stack.Screen name="BirthdayEntryPage" component={BirthdayEntryPage} />
-        <Stack.Screen
-          name="GenderIdentityPage"
-          component={GenderIdentityPage}
-        />
-        <Stack.Screen
-          name="DatingInterestPage"
-          component={DatingInterestPage}
-        />
-        <Stack.Screen name="SchoolEntryPage" component={SchoolEntryPage} />
-        <Stack.Screen
-          name="PoliticalEntryPage"
-          component={PoliticalEntryPage}
-        />
-        <Stack.Screen name="ReligionEntryPage" component={ReligionEntryPage} />
-        <Stack.Screen
-          name="EthnicityEntryPage"
-          component={EthnicityEntryPage}
-        />
-        <Stack.Screen
-          name="OccupationEntryPage"
-          component={OccupationEntryPage}
-        />
-        <Stack.Screen name="LocationEntryPage" component={LocationEntryPage} />
-        <Stack.Screen name="BioEntryPage" component={BioEntryPage} />
-        <Stack.Screen
-          name="PhotoSelectionPage"
-          component={PhotoSelectionPage}
-        />
-        <Stack.Screen name="PromptEntryPage" component={PromptEntryPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <SignUpNavigator />;
 };
 
 export default App;
